ce5978d410ca9c128c5aee42f81f3fa494df25b8
Eltrefy <93093911+Eltrefy@users.noreply.github.com>
2022-02-14 09:08:23 +0300

    Delete napalm_code.py

--- napalm_code.py
+++
@@ -1,9 +0,0 @@
-#code for saving running configuration using pyntc
-import json
-from pyntc import ntc_device as ntc
-ios_device = ntc(host='192.168.1.100', username='cisco123',
-                 password='cisco123', device_type='cisco_ios_ssh', secret="cisco")#secret is for enable password ||secret
-mytxt = open(
-    r"E:\New folder\New folder\Automation\cisco _programming\my_pyntc2.txt", "w") #
-writerr = ios_device.running_config
-mytxt.write(writerr)       
