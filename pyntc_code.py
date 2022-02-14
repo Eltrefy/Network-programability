52b3769166dbbc8927618369ac3b38ae7b898c10
mohamed <mhmdmmdwhw22@gmail.com>
2022-02-14 09:24:59 +0300

    adding comments to the code

--- pyntc_code.py
+++ pyntc_code.py
@@ -4,6 +4,6 @@ from pyntc import ntc_device as ntc
 ios_device = ntc(host='192.168.1.100', username='cisco123',
                  password='cisco123', device_type='cisco_ios_ssh', secret="cisco")#secret is for enable password ||secret
 mytxt = open(
-    r"E:\New folder\New folder\Automation\cisco _programming\my_pyntc2.txt", "w") #
+    r"E:\New folders\New folders\my_configuration.txt", "w") #add your own path to configuration txt
 writerr = ios_device.running_config
 mytxt.write(writerr)       
